544eaab4c5b04def733bb33c7b911dae94318823
AbbasiMohamad <m.abbasi.72@gmail.com>
2024-10-29 16:14:45 +0330

    initial commit

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+// application status, operating environment and version.
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
+}
